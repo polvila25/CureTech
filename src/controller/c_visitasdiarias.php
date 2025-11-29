d9096aba82a7c0e2d64255152fa5c60eddd76320
polvila25 <polvila25@gmail.com>
2025-11-29 14:14:36 +0100

    visitas diarias

--- src/controller/c_visitasdiarias.php
+++ src/controller/c_visitasdiarias.php
@@ -1,1 +1,9 @@
-<?php require __DIR__. '/../view/v_visitasdiarias.php'; ?>
+<?php 
+    require __DIR__. '/../view/v_visitasdiarias.php'; 
+    require_once __DIR__. '/../model/m_conectaDB.php';
+    require_once __DIR__. '/../model/m_getpacientes.php';
+
+    $pacientes = getPacientes();
+
+    require __DIR__ '/../view/visitasdiaris.php';
+?>
