df08712a95623f1f339406b34786178f65b4065c
polvila25 <polvila25@gmail.com>
2025-11-29 16:10:26 +0100

    subir imagenes y vista mmejorada

--- src/view/v_pacientes.php
+++ src/view/v_pacientes.php
@@ -4,7 +4,20 @@
             <div class="paciente-box">
                 <h3><?php echo $paciente['nombre']; ?></h3>
                 <p>Edad: <?php echo $paciente['edad']; ?> años</p>
-                <p>Estado: <span class="estado-<?php echo strtolower($paciente['estado_paciente']); ?>"><?php echo $paciente['estado_paciente']; ?></span></p>
+
+                <p>Estado:
+                    <?php 
+                        $estado = strtolower($paciente['estado_paciente']); 
+                        $icono = "images/" . $estado . ".png"; 
+                    ?>
+
+                    <span class="estado-<?php echo $estado; ?>">
+                        <?php echo $paciente['estado_paciente']; ?>
+                    </span>
+                    <img src="<?php echo $icono; ?>" alt="<?php echo $estado; ?>" class="icono-estado">
+
+                </p>
+
                 <p>Última visita: <?php echo $paciente['fecha_ultima_visita']; ?></p>
             </div>
         </a>
